eacc5865eb015e41e4ec6b5963baac6a8c026985
Andrew Ralston <andrewralston35@gmail.com>
2020-06-12 10:21:05 +0000

    Updated server settings

--- run.py
+++ run.py
@@ -46,6 +46,5 @@ def careers():
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+    app.run(host=os.environ.get("IP", '0.0.0.0'),
+            port=int(os.environ.get("PORT", "5000")))
